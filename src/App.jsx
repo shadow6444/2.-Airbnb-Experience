fec42104a009448f7acd9cca886b581bbc4e19c5
shadow6444 <shadowgarden644@gmail.com>
2024-09-20 15:40:57 +0500

    Changed Title

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,7 @@ import Navbar from "./components/Navbar.jsx";
 import MainHero from "./components/MainHero.jsx";
 import Card from "./Card.jsx";
 import data from "./data.js";
-/*
-Challenge:
 
-- import the array of data from data.js
-- map over the array to create <Card /> components
-- display the array of card components under the navbar
-  (in place of the current <Card /> component)
-
-Note: We haven't styled the group of components yet, so they'll
-still be block elements, stacked vertically. We'll add styling later.
-*/
 
 function App() {
   const cards = data.map((item) => {
